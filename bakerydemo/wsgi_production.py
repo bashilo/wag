d312b2402106bd567adf72bd7762bb5383e64f48
bashilo <bashilo@gmail.com>
2018-06-03 23:52:02 +0900

    debug

--- bakerydemo/wsgi_production.py
+++ bakerydemo/wsgi_production.py
@@ -1,5 +1,11 @@
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+import os
 
+import dotenv
+from django.core.wsgi import get_wsgi_application
+
+
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
